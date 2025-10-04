3d36065be0e8e199fae50449f679b29f05689deb
Rajib chandra Das <prorajib6@gmail.com>
2025-10-04 22:46:39 +0600

    appolo server setup

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginCacheControl } from "@apollo/server/plugin/cacheControl";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
 const typeDefs = `#graphql
-   type Query {
-    greeting: String
-    
-   }
+  schema{
+   query:Query
+  }
+  type Query {
+   greeting: String
+  }
 `;
 
+
+
+const resolvers ={
+
+   Query:{
+      greeting:()=> "Hello world!"
+   }
+}
+
+
+
+const server = new ApolloServer({typeDefs,resolvers}); 
+
+
+const {url} = await startStandaloneServer(server, {listen:{port:9000}});
+
+console.log(`Server is running at ${url}`);
 
 
